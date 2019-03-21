aa9b6348680587063b2d009ae440f2f194dd9f17
keiya shimomura <skeiya@gmail.com>
2019-03-21 15:43:11 +0900

    タスク管理を追加。

---
+++ TaskManagement/Period.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManagement
+{
+    public class Period
+    {
+        private readonly IPeriodCalculator _periodCalculator;
+
+        public Period(CallenderDay from, CallenderDay to, IPeriodCalculator periodCalculator)
+        {
+            this.From = from;
+            this.To = to;
+            _periodCalculator = periodCalculator;
+        }
+
+        public CallenderDay From { set; get; }
+        public CallenderDay To { set; get; }
+
+        public override string ToString()
+        {
+            return _periodCalculator.GetTerm(From, To).ToString();
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,9 +8,9 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestCallenderTayFormat()
+        public void TestCallenderDayFormat()
         {
-            var day = new CallenderDay() { Year = 2019, Month = 1, Day = 1 };
+            var day = new CallenderDay(2019, 1, 1);
             Assert.AreEqual<string>("2019/01/01", day.ToString());
         }
 
@@ -19,6 +19,23 @@ namespace UnitTestProject1
         {
             var member = new Member("下村", "圭矢", "AA");
             Assert.AreEqual<string>("下圭(AA)", member.ToString());
+        }
+
+        [TestMethod]
+        public void TestCallernderDayEquals()
+        {
+            var c1 = new CallenderDay(2019, 3, 3);
+            var c2 = new CallenderDay(2019, 3, 3);
+            var c3 = new CallenderDay(2019, 3, 4);
+            Assert.IsTrue(c1.Equals(c2));
+            Assert.IsFalse(c1.Equals(c3));
+        }
+
+        [TestMethod]
+        public void TestWorkItemFormat()
+        {
+            var wi = new WorkItem("Z123", "仕様検討", new Period(new CallenderDay(2019, 3, 20), new CallenderDay(2019, 3, 22), new DummyPeriodCalculator()));
+            Assert.AreEqual<string>("仕様検討 Z123 3d", wi.ToString());
         }
     }
 }
