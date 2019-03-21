4e154506191eb119f0815e681393d20124c96d2e
keiya shimomura <skeiya@gmail.com>
2019-03-21 11:41:06 +0900

    メンバ管理を追加。

---
+++ TaskManagement/Team.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManagement
+{
+    public class Team
+    {
+        private List<Member> _members = new List<Member>();
+
+        public Team()
+        {
+            _members.Add(new Member("下村", "圭矢", "K"));
+            _members.Add(new Member("hoge", "foo", "AB"));
+            _members.Add(new Member("avd", "rg", "AB"));
+        }
+
+        public List<Member> Members => _members;
+    }
+}
