1cccdf474986a9164e6c97520ac375fe2e2c0aac
keiya shimomura <skeiya@gmail.com>
2019-04-30 00:10:40 +0900

    保存時に領域の重なりを警告表示するようにした。

---
+++ TaskManagement/Logic/OverwrapedWorkItemsGetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using TaskManagement.Model;
+
+namespace TaskManagement.Logic
+{
+    public class OverwrapedWorkItemsGetter
+    {
+        public static List<WorkItem> Get(WorkItems workItems)
+        {
+            var result = new List<WorkItem>();
+            foreach (var src in workItems)
+            {
+                foreach (var dst in workItems)
+                {
+                    if (src.Equals(dst))
+                    {
+                        continue;
+                    }
+                    if (!src.AssignedMember.Equals(dst.AssignedMember)) continue;
+                    if (src.Period.HasInterSection(dst.Period)) result.Add(src);
+                }
+            }
+            return result;
+        }
+    }
+}
