bc521cfb859fd5916e8dc262f63d74f2ce6d6c74
keiya shimomura <skeiya@gmail.com>
2019-06-11 21:51:46 +0900

    フィルターダイアログのリサイズに対応。

--- TaskManagement/UI/FilterForm.Designer.cs
+++ TaskManagement/UI/FilterForm.Designer.cs
@@ -48,7 +48,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 52);
+            this.label1.Location = new System.Drawing.Point(6, 52);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 12);
@@ -57,24 +57,28 @@
             // 
             // textBoxFrom
             // 
-            this.textBoxFrom.Location = new System.Drawing.Point(26, 74);
-            this.textBoxFrom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFrom.Location = new System.Drawing.Point(22, 74);
+            this.textBoxFrom.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxFrom.Name = "textBoxFrom";
-            this.textBoxFrom.Size = new System.Drawing.Size(110, 19);
+            this.textBoxFrom.Size = new System.Drawing.Size(168, 19);
             this.textBoxFrom.TabIndex = 4;
             // 
             // textBoxTo
             // 
-            this.textBoxTo.Location = new System.Drawing.Point(26, 108);
-            this.textBoxTo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTo.Location = new System.Drawing.Point(22, 108);
+            this.textBoxTo.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxTo.Name = "textBoxTo";
-            this.textBoxTo.Size = new System.Drawing.Size(110, 19);
+            this.textBoxTo.Size = new System.Drawing.Size(168, 19);
             this.textBoxTo.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 134);
+            this.label2.Location = new System.Drawing.Point(6, 134);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 12);
@@ -83,19 +87,23 @@
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(11, 154);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(9, 154);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(200, 228);
+            this.checkedListBox1.Size = new System.Drawing.Size(227, 200);
             this.checkedListBox1.TabIndex = 10;
             // 
             // buttonApply
             // 
-            this.buttonApply.Location = new System.Drawing.Point(61, 414);
-            this.buttonApply.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(107, 388);
+            this.buttonApply.Margin = new System.Windows.Forms.Padding(2);
             this.buttonApply.Name = "buttonApply";
-            this.buttonApply.Size = new System.Drawing.Size(72, 23);
+            this.buttonApply.Size = new System.Drawing.Size(62, 23);
             this.buttonApply.TabIndex = 11;
             this.buttonApply.Text = "OK";
             this.buttonApply.UseVisualStyleBackColor = true;
@@ -103,11 +111,12 @@
             // 
             // buttonCancel
             // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(136, 414);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCancel.Location = new System.Drawing.Point(172, 388);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(72, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(62, 23);
             this.buttonCancel.TabIndex = 0;
             this.buttonCancel.Text = "閉じる";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -116,7 +125,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 4);
+            this.label3.Location = new System.Drawing.Point(5, 4);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 12);
@@ -125,16 +134,18 @@
             // 
             // textBoxWorkItem
             // 
-            this.textBoxWorkItem.Location = new System.Drawing.Point(26, 26);
-            this.textBoxWorkItem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxWorkItem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxWorkItem.Location = new System.Drawing.Point(22, 26);
+            this.textBoxWorkItem.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxWorkItem.Name = "textBoxWorkItem";
-            this.textBoxWorkItem.Size = new System.Drawing.Size(110, 19);
+            this.textBoxWorkItem.Size = new System.Drawing.Size(168, 19);
             this.textBoxWorkItem.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(64, 94);
+            this.label4.Location = new System.Drawing.Point(55, 94);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(23, 12);
@@ -143,10 +154,11 @@
             // 
             // buttonClearWorkItem
             // 
-            this.buttonClearWorkItem.Location = new System.Drawing.Point(162, 26);
-            this.buttonClearWorkItem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonClearWorkItem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClearWorkItem.Location = new System.Drawing.Point(194, 26);
+            this.buttonClearWorkItem.Margin = new System.Windows.Forms.Padding(2);
             this.buttonClearWorkItem.Name = "buttonClearWorkItem";
-            this.buttonClearWorkItem.Size = new System.Drawing.Size(46, 22);
+            this.buttonClearWorkItem.Size = new System.Drawing.Size(39, 22);
             this.buttonClearWorkItem.TabIndex = 2;
             this.buttonClearWorkItem.Text = "クリア";
             this.buttonClearWorkItem.UseVisualStyleBackColor = true;
@@ -154,10 +166,11 @@
             // 
             // buttonClearPeriod
             // 
-            this.buttonClearPeriod.Location = new System.Drawing.Point(162, 74);
-            this.buttonClearPeriod.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonClearPeriod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClearPeriod.Location = new System.Drawing.Point(194, 74);
+            this.buttonClearPeriod.Margin = new System.Windows.Forms.Padding(2);
             this.buttonClearPeriod.Name = "buttonClearPeriod";
-            this.buttonClearPeriod.Size = new System.Drawing.Size(46, 22);
+            this.buttonClearPeriod.Size = new System.Drawing.Size(39, 22);
             this.buttonClearPeriod.TabIndex = 7;
             this.buttonClearPeriod.Text = "クリア";
             this.buttonClearPeriod.UseVisualStyleBackColor = true;
@@ -165,10 +178,11 @@
             // 
             // buttonClearMembers
             // 
-            this.buttonClearMembers.Location = new System.Drawing.Point(162, 130);
-            this.buttonClearMembers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonClearMembers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClearMembers.Location = new System.Drawing.Point(194, 130);
+            this.buttonClearMembers.Margin = new System.Windows.Forms.Padding(2);
             this.buttonClearMembers.Name = "buttonClearMembers";
-            this.buttonClearMembers.Size = new System.Drawing.Size(46, 20);
+            this.buttonClearMembers.Size = new System.Drawing.Size(39, 20);
             this.buttonClearMembers.TabIndex = 9;
             this.buttonClearMembers.Text = "クリア";
             this.buttonClearMembers.UseVisualStyleBackColor = true;
@@ -176,10 +190,11 @@
             // 
             // buttonImport
             // 
-            this.buttonImport.Location = new System.Drawing.Point(10, 389);
-            this.buttonImport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonImport.Location = new System.Drawing.Point(64, 363);
+            this.buttonImport.Margin = new System.Windows.Forms.Padding(2);
             this.buttonImport.Name = "buttonImport";
-            this.buttonImport.Size = new System.Drawing.Size(99, 20);
+            this.buttonImport.Size = new System.Drawing.Size(85, 20);
             this.buttonImport.TabIndex = 9;
             this.buttonImport.Text = "インポート...";
             this.buttonImport.UseVisualStyleBackColor = true;
@@ -187,10 +202,11 @@
             // 
             // buttonExport
             // 
-            this.buttonExport.Location = new System.Drawing.Point(112, 390);
-            this.buttonExport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExport.Location = new System.Drawing.Point(151, 364);
+            this.buttonExport.Margin = new System.Windows.Forms.Padding(2);
             this.buttonExport.Name = "buttonExport";
-            this.buttonExport.Size = new System.Drawing.Size(96, 20);
+            this.buttonExport.Size = new System.Drawing.Size(82, 20);
             this.buttonExport.TabIndex = 9;
             this.buttonExport.Text = "エクスポート...";
             this.buttonExport.UseVisualStyleBackColor = true;
@@ -199,10 +215,10 @@
             // FilterForm
             // 
             this.AcceptButton = this.buttonApply;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(215, 444);
+            this.ClientSize = new System.Drawing.Size(239, 418);
             this.Controls.Add(this.buttonExport);
             this.Controls.Add(this.buttonImport);
             this.Controls.Add(this.buttonClearMembers);
@@ -218,8 +234,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FilterForm";
             this.ShowIcon = false;
             this.Text = "フィルター";
