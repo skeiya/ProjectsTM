0adfd7909fd7d84f6c7e7efa6dfcf3e3abe5e8f3
matsukage <matsukage_home@outlook.jp>
2020-06-20 00:44:45 +0900

    タスクリスト上の値を編集できてしまう問題を修正

--- TaskManagement/UI/TaskList.Designer.cs
+++ TaskManagement/UI/TaskList.Designer.cs
@@ -37,6 +37,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 21;
             this.dataGridView1.Size = new System.Drawing.Size(776, 426);
             this.dataGridView1.TabIndex = 1;
