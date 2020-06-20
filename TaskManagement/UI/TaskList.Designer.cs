3aa4438127ede4643eb8e2ff6b72b97a5752f55a
matsukage <matsukage_home@outlook.jp>
2020-06-20 11:26:42 +0900

    ダイアログの大きさを変えてもDataGridViewの大きさがが変わらない問題を修正

--- TaskManagement/UI/TaskList.Designer.cs
+++ TaskManagement/UI/TaskList.Designer.cs
@@ -34,6 +34,10 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top 
+            | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
