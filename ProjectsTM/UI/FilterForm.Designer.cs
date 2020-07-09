34e44100a21dd621273577a47d3effadb3916dc1
keiya shimomura <skeiya@gmail.com>
2020-07-09 23:43:07 +0900

    アンカー設定の漏れを修正。

--- ProjectsTM/UI/FilterForm.Designer.cs
+++ ProjectsTM/UI/FilterForm.Designer.cs
@@ -284,6 +284,8 @@
             // 
             // comboBoxPattern
             // 
+            this.comboBoxPattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxPattern.FormattingEnabled = true;
             this.comboBoxPattern.Location = new System.Drawing.Point(22, 26);
             this.comboBoxPattern.Name = "comboBoxPattern";
--- ProjectsTM/UI/SearchWorkitemForm.Designer.cs
+++ ProjectsTM/UI/SearchWorkitemForm.Designer.cs
@@ -150,6 +150,8 @@
             // 
             // comboBoxPattern
             // 
+            this.comboBoxPattern.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxPattern.FormattingEnabled = true;
             this.comboBoxPattern.Location = new System.Drawing.Point(10, 8);
             this.comboBoxPattern.Name = "comboBoxPattern";
