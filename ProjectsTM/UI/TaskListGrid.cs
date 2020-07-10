7c3ae1f6e6293346af7d609f6b8f5663bc3f0c4b
keiya shimomura <skeiya@gmail.com>
2020-07-11 00:49:13 +0900

    タスクリストの最終行が自動リサイズするように変更。

--- ProjectsTM/UI/TaskListGrid.cs
+++ ProjectsTM/UI/TaskListGrid.cs
@@ -18,6 +18,19 @@ namespace ProjectsTM.UI
         {
             InitializeComponent();
             this.OnDrawNormalArea += TaskListGrid_OnDrawNormalArea;
+            this.Resize += TaskListGrid_Resize;
+        }
+
+        private void TaskListGrid_Resize(object sender, EventArgs e)
+        {
+            UpdateLastColWidth();
+        }
+
+        private void UpdateLastColWidth()
+        {
+            if (ColWidths.Count == 0) return;
+            if (ColWidths.Count < ColCount) return;
+            ColWidths[ColCount - 1] = GetLastColWidth();
         }
 
         internal void Initialize(ViewData viewData)
@@ -28,15 +41,28 @@ namespace ProjectsTM.UI
             var colWidth = (int)Math.Ceiling(g.MeasureString("2000A12A31", font).Width);
             var memberHeight = (int)Math.Ceiling(g.MeasureString("NAME", font).Height);
             var height = (int)(memberHeight);
-            foreach (var c in ColIndex.Range(0, ColCount))
+            foreach (var c in ColIndex.Range(0, ColCount - 1))
             {
                 ColWidths[c.Value] = colWidth;
             }
+            UpdateLastColWidth();
             foreach (var r in RowIndex.Range(0, RowCount))
             {
                 RowHeights[r.Value] = height;
             }
             LockUpdate = false;
+        }
+
+        private float GetLastColWidth()
+        {
+            if (ColCount == 0) return 0;
+            if (ColCount == 1) return Width;
+            var rest = 0f;
+            for (var idx = 0; idx < ColCount - 1; idx++)
+            {
+                rest += ColWidths[idx];
+            }
+            return Width - rest - 17/*スクロールバーの幅*/ - 2;
         }
 
         private void TaskListGrid_OnDrawNormalArea(object sender, FreeGridControl.DrawNormalAreaEventArgs e)
