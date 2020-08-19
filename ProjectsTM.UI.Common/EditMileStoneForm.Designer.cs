292691e13a04083358bb597728973d29890bc811
keiya shimomura <skeiya@gmail.com>
2020-08-19 22:48:33 +0900

    マイルストーンに状態を持たせるように対応。

--- ProjectsTM.UI.Common/EditMileStoneForm.Designer.cs
+++ ProjectsTM.UI.Common/EditMileStoneForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ProjectsTM.UI
+﻿namespace ProjectsTM.UI.Common
 {
     partial class EditMileStoneForm
     {
@@ -38,6 +38,8 @@
             this.buttonSelectColor = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -75,7 +77,7 @@
             this.textBoxName.Location = new System.Drawing.Point(72, 6);
             this.textBoxName.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(144, 19);
+            this.textBoxName.Size = new System.Drawing.Size(141, 19);
             this.textBoxName.TabIndex = 1;
             // 
             // textBoxDate
@@ -83,27 +85,27 @@
             this.textBoxDate.Location = new System.Drawing.Point(72, 32);
             this.textBoxDate.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.textBoxDate.Name = "textBoxDate";
-            this.textBoxDate.Size = new System.Drawing.Size(144, 19);
+            this.textBoxDate.Size = new System.Drawing.Size(141, 19);
             this.textBoxDate.TabIndex = 3;
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(72, 110);
+            this.buttonOK.Location = new System.Drawing.Point(72, 139);
             this.buttonOK.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(66, 26);
-            this.buttonOK.TabIndex = 6;
+            this.buttonOK.TabIndex = 10;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(140, 110);
+            this.buttonCancel.Location = new System.Drawing.Point(140, 139);
             this.buttonCancel.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(73, 26);
-            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.TabIndex = 11;
             this.buttonCancel.Text = "キャンセル";
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
@@ -124,8 +126,8 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(72, 85);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Size = new System.Drawing.Size(141, 20);
+            this.comboBox1.TabIndex = 7;
             // 
             // label3
             // 
@@ -135,14 +137,34 @@
             this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 20);
-            this.label3.TabIndex = 9;
+            this.label3.TabIndex = 6;
             this.label3.Text = "マイルストーン\r\nフィルタ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "状態";
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Location = new System.Drawing.Point(72, 111);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(141, 20);
+            this.comboBoxState.TabIndex = 9;
             // 
             // EditMileStoneForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(219, 144);
+            this.ClientSize = new System.Drawing.Size(219, 176);
+            this.Controls.Add(this.comboBoxState);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonSelectColor);
@@ -175,5 +197,7 @@
         private System.Windows.Forms.Button buttonSelectColor;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxState;
     }
 }
