63dad8aeef09b2ee41c70ea6a08059ce088458dd
keiya shimomura <skeiya@gmail.com>
2020-08-22 08:37:52 +0900

    レビュー指摘対応。

--- ProjectsTM.Service/OverwrapedWorkItemsCollectService.cs
+++ ProjectsTM.Service/OverwrapedWorkItemsCollectService.cs
@@ -19,11 +19,20 @@ namespace ProjectsTM.Service
                     {
                         var p1 = ar[idx1].Period;
                         var p2 = ar[idx2].Period;
-                        if (p2.From <= p1.To) result.Add(ar[idx1]);
+                        if (p2.From <= p1.To)
+                        {
+                            Add(result, ar[idx1], ar[idx2]);
+                        }
                     }
                 }
             }
             return result;
+        }
+
+        private static void Add(List<WorkItem> result, WorkItem w1, WorkItem w2)
+        {
+            if (!result.Any(r => r.Equals(w1))) result.Add(w1);
+            if (!result.Any(r => r.Equals(w2))) result.Add(w2);
         }
     }
 }
