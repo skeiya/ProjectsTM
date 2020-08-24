fc74c9752cbb0aa7f0ad7f413da3c3f17e69e10e
keiya shimomura <skeiya@gmail.com>
2020-08-24 22:30:16 +0900

    System.ValueTupleを不要にした。

---
+++ ProjectsTM.UI.MainForm/RawRange.cs
@@ -0,0 +1,21 @@
+﻿using FreeGridControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsTM.UI.MainForm
+{
+    class RawRange
+    {
+        public RowIndex Row { get; private set; }
+        public int Count { get; private set; }
+
+        public RawRange(RowIndex row, int count)
+        {
+            this.Row = row;
+            this.Count = count;
+        }
+    }
+}
