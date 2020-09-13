cbf580f752865956941d03aa018833fe90a50d52
matsukage <matsukage_home@outlook.jp>
2020-09-13 19:19:30 +0900

    MemberState設定ComboBoxのDropDownStyleをDropDownListへ変更

--- ProjectsTM.UI.Common/EditMemberForm.Designer.cs
+++ ProjectsTM.UI.Common/EditMemberForm.Designer.cs
@@ -72,6 +72,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(299, 8);
             this.comboBox1.Name = "comboBox1";
