49a32bdad691b46d520135781612d88184e07a92
naoto akashi <aksh0402@outlook.jp>
2020-10-04 18:07:25 +0900

    修正2
    コミット漏れ

--- ProjectsTM.Service/CountMemberNumService.cs
+++ ProjectsTM.Service/CountMemberNumService.cs
@@ -1,41 +1,23 @@
 ﻿using ProjectsTM.Model;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Runtime.CompilerServices;
 
 namespace ProjectsTM.Service
 {
     public static class CountMemberNumService
     {
-        public  static string GetCountStr(Members members )
+        public  static string GetCountStr(Members members)
         {
-            var temp = new HashSet<string>();
-            foreach (var m in members)
-            {
-                var belengTo = m.Company;
-                temp.Add(belengTo);
-            }
-            var companyList = temp.ToArray();
+            var companyList = GetCompanyList(members);
 
-            var countList = new List<int>();
-            int count;
-            foreach (var i in companyList)
-            {
-                count = 0;
-                foreach (var m in members)
-                {
-                    if (i.Equals(m.Company))
-                    {
-                        count++;
-                    }
-                }
-                countList.Add(count);
-            }
-            count = 0;
+            var countList = GetCountList(members, companyList).ToList();
+
+            var count = 0;
             string dispStr = string.Format("Total:{0} (", members.Count);
-            foreach (var i in companyList)
+            foreach (var company in companyList)
             {
-                dispStr += Concat(i, countList[count]);
+                dispStr += Concat(company, countList[count]);
                 count++;
             }
             return dispStr + ")";
@@ -45,5 +27,36 @@ namespace ProjectsTM.Service
         {
             return $"{belongTo}:{count} ";
         }
+
+        private static IEnumerable<string> GetCompanyList(Members members)
+        {
+            var companyList = new HashSet<string>();
+            foreach (var m in members)
+            {
+                var belengTo = m.Company;
+                companyList.Add(belengTo);
+            }
+            return companyList;
+        }
+
+        private static IEnumerable<int> GetCountList(Members members, IEnumerable<string> companyList)
+        {
+            var countList = new List<int>();
+            int count;
+            foreach (var company in companyList)
+            {
+                count = 0;
+                foreach (var member in members)
+                {
+                    if (company.Equals(member.Company))
+                    {
+                        count++;
+                    }
+                }
+                countList.Add(count);
+            }
+            return countList;
+        }
+
     }
 }
