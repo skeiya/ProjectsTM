820df592d5248119072ce74dfcb611f8d5b0696f
matsukage <matsukage_home@outlook.jp>
2020-11-01 08:54:45 +0900

    ユーザ名を選択するフォームを追加

---
+++ ProjectsTM.UI.MainForm/ManageMySettingForm.cs
@@ -0,0 +1,35 @@
+﻿using ProjectsTM.Model;
+using System.Windows.Forms;
+
+namespace ProjectsTM.UI.MainForm
+{
+    public partial class ManageMySettingForm : Form
+    {
+        private readonly Members _members;
+        public string Selected => comboBox1.SelectedItem.ToString();
+
+        public ManageMySettingForm(Members members, string userName)
+        {
+            _members = members;
+            InitializeComponent();
+            InitCombo(userName);
+        }
+
+        private void InitCombo(string userName)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("未設定");
+            bool found = false;
+            foreach(var m in _members)
+            {
+                comboBox1.Items.Add(m.ToString());
+                if (userName.Equals(m.ToString()))
+                {
+                    comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+                    found = true;
+                }
+            }
+            if(!found) comboBox1.SelectedIndex = 0;
+        }
+    }
+}
