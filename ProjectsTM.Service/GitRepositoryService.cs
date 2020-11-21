b3762ab9a4d70525af690f943b61f5d4f38b10d5
matsukage <matsukage_home@outlook.jp>
2020-11-21 15:50:10 +0900

    不要コード削除

--- ProjectsTM.Service/GitRepositoryService.cs
+++ ProjectsTM.Service/GitRepositoryService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace ProjectsTM.Service
 {
     public static class GitRepositoryService
-    {        
+    {
         public static Task<bool> HasUnmergedRemoteCommit(string filePath)
         {
             Task<bool> task = Task.Run(() =>
