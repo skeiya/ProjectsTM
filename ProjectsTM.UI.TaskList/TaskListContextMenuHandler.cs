171b2c7a7580e15b081c89469cca531b07a6573f
naoto akashi <aksh0402@outlook.jp>
2020-12-05 17:59:32 +0900

    タスクリストの右クリックメニュー対応
    ■　TaskListContextMenuHandlerを追加。
    ・　状態をDoneに変える。
    
    ■　TaskListFormにメンバTaskListContextMenuHandlerを追加。

---
+++ ProjectsTM.UI.TaskList/TaskListContextMenuHandler.cs
@@ -0,0 +1,35 @@
+﻿using ProjectsTM.Model;
+using ProjectsTM.ViewModel;
+using System;
+using System.Windows.Forms;
+
+namespace ProjectsTM.UI.TaskList
+{
+    class TaskListContextMenuHandler
+    {
+        private ViewData _viewData;
+        private TaskListGrid _taskListGrid;
+
+        public TaskListContextMenuHandler(ViewData viewData, TaskListGrid taskListGrid)
+        {
+            _viewData = viewData;
+            _taskListGrid = taskListGrid;
+
+            taskListGrid.ContextMenuStrip = new ContextMenuStrip();
+            taskListGrid.ContextMenuStrip.Items.Add(new ToolStripMenuItem("→状態；Done", null, DoneMenu_Click, Keys.Control | Keys.D));
+        }
+
+        private void DoneMenu_Click(object sender, EventArgs e)
+        {
+            ChangeState(TaskState.Done);
+            _taskListGrid.UpdateView();
+        }
+
+        private void ChangeState(TaskState state)
+        {
+            var selected = _viewData.Selected;
+            if (selected == null) return;
+            _taskListGrid.EditService.ChangeState(selected, state);
+        }
+    }
+}
