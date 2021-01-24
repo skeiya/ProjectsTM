7a169bd643bbf6f806ef0eb2618674f5b6cb100b
naoto akashi <aksh0402@outlook.jp>
2021-01-24 09:42:09 +0900

    タスクをパッと見れるようにする対応
    ブランチ切り直しました。

--- ProjectsTM.UI.MainForm/MainForm.cs
+++ ProjectsTM.UI.MainForm/MainForm.cs
@@ -452,7 +452,7 @@ namespace ProjectsTM.UI.MainForm
         {
             if (TaskListForm == null || TaskListForm.IsDisposed)
             {
-                TaskListForm = new TaskListForm(_viewData, _patternHistory);
+                TaskListForm = new TaskListForm(_viewData, _userName, _patternHistory);
             }
             if (!TaskListForm.Visible) TaskListForm.Show(this);
         }
--- ProjectsTM.UI.TaskList/TaskListForm.cs
+++ ProjectsTM.UI.TaskList/TaskListForm.cs
@@ -12,13 +12,15 @@ namespace ProjectsTM.UI.TaskList
     {
         private readonly ViewData _viewData;
         private PatternHistory _history;
+        private readonly string _userName;
         private TaskListContextMenuHandler _taskListContextMenuHandler;
 
-        public TaskListForm(ViewData viewData, PatternHistory patternHistory)
+        public TaskListForm(ViewData viewData, string userName, PatternHistory patternHistory)
         {
             InitializeComponent();
 
             this._viewData = viewData;
+            _userName = userName;
            _taskListContextMenuHandler = new TaskListContextMenuHandler(viewData, gridControl1);
             this._history = patternHistory;
             gridControl1.ListUpdated += GridControl1_ListUpdated;
@@ -28,6 +30,7 @@ namespace ProjectsTM.UI.TaskList
             this.FormClosed += TaskListForm_FormClosed;
             this.checkBoxShowMS.CheckedChanged += CheckBoxShowMS_CheckedChanged;
             this.buttonEazyRegex.Click += buttonEazyRegex_Click;
+            comboBoxPattern.SelectedIndexChanged += comboBoxPattern_SelectedIndexChanged;
         }
 
         private void buttonEazyRegex_Click(object sender, EventArgs e)
@@ -102,8 +105,13 @@ namespace ProjectsTM.UI.TaskList
         private void comboBoxPattern_DropDown(object sender, System.EventArgs e)
         {
             comboBoxPattern.Items.Clear();
+            if (IsPersonalSettingSet(_userName))
+            {
+                comboBoxPattern.Items.Add($"あなた({_userName})のタスク");
+            }
             comboBoxPattern.Items.AddRange(_history.Items.ToArray());
         }
+
         private void buttonUpdate_Click(object sender, System.EventArgs e)
         {
             UpdateList();
@@ -114,6 +122,29 @@ namespace ProjectsTM.UI.TaskList
             _history.Append(comboBoxPattern.Text);
             gridControl1.Option = GetOption();
             gridControl1.UpdateView();
+        }
+
+        private void comboBoxPattern_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IsUserTaskSortSelected()) return;
+            gridControl1.Option = new TaskListOption(AdjustUserNameFormat(_userName), false, string.Empty);
+            gridControl1.UpdateView();
+            comboBoxPattern.SelectedIndex = -1;
+        }
+        private bool IsUserTaskSortSelected()
+        {
+            return comboBoxPattern.SelectedIndex != 0 || !IsPersonalSettingSet(_userName);
+        }
+
+        private string AdjustUserNameFormat(string userName)
+        {
+            var adjustedUserName = userName.Replace("(", @"\(").Replace(")", @"\)");
+            return adjustedUserName;
+        }
+
+        private bool IsPersonalSettingSet(string userName)
+        {
+            return !userName.Equals("未設定");
         }
     }
 }
