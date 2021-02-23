87cccdcb5c3d7808a4230e1bbd0c7bb982e3bc4c
keiya shimomura <skeiya@gmail.com>
2021-02-23 14:08:09 +0900

    コードのクリーンナップを実行。

--- ProjectsTM.Model/WorkItems.cs
+++ ProjectsTM.Model/WorkItems.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Xml;
 using System.Xml.Linq;
 
 namespace ProjectsTM.Model
--- ProjectsTM.Service/EditorFindService.cs
+++ ProjectsTM.Service/EditorFindService.cs
@@ -1,6 +1,5 @@
 ﻿using ProjectsTM.Model;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProjectsTM.Service
